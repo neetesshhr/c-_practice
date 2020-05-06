d911c002a8eb466d00d1ed8b9f493785d4ed7dd3
neetesshhr <rijalboy.94@gmail.com>
2020-05-06 20:06:11 +0545

    ff

---
+++ TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var number = 2;
+           var count = 10;
+           var totalPrice = 20.95f;
+           var character = 'N';
+           var firstname = "Nitesh";
+           var isWorking = true;
+           System.Console.WriteLine(number);
+           System.Console.WriteLine(count);
+           System.Console.WriteLine(totalPrice);
+           System.Console.WriteLine(character);
+           System.Console.WriteLine(firstname);
+           System.Console.WriteLine(isWorking);     
+          
+        }
+    }
+}
